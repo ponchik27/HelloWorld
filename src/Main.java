96e60547402d6cc7b16c8b5dccddc3861f8005aa
ponchik18 <maks.bovkun.228.111@gmail.com>
2023-09-23 18:04:01 +0300

    Change from hotfixes/fork

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world2!");
         System.out.println("Hello world3!");
+        System.out.println("Hello world from fork!");
+        System.out.println("Hello world from fork2!");
     }
 }
